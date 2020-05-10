2f80e0b671071e5994f57c26d81fdb7718cd750a
Sean Robertson <srobertson421@gmail.com>
2020-05-10 16:43:39 -0700

    Added tilemap for dirt paths. Added grass background. Added follow camera for player

--- blueprints/Player/Player.gd
+++ blueprints/Player/Player.gd
@@ -13,8 +13,8 @@ onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
